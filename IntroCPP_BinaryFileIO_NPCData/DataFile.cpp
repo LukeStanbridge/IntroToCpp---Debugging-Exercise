c48162b27c03caf5b0fe7f8d8a60e117cc2486d6
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2022-05-11 16:03:26 +0930

    setup debugging project

--- IntroCPP_BinaryFileIO_NPCData/DataFile.cpp
+++ IntroCPP_BinaryFileIO_NPCData/DataFile.cpp
@@ -2,6 +2,29 @@
 #include <fstream>
 using namespace std;
 
+Image LoadImageEx(Color* pixels, int width, int height)
+{
+	Image image = { 0 };
+	unsigned char* fileData = (unsigned char*)pixels;
+	unsigned char* dataPtr = fileData;
+	unsigned int size = GetPixelDataSize(width, height, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+	image.data = RL_MALLOC(size);      // Allocate required memory in bytes
+	if (image.data)
+	{
+		memcpy(image.data, dataPtr, size); // Copy required data to image
+		image.width = width;
+		image.height = height;
+		image.mipmaps = 1;
+		image.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+	}
+	return image;
+}
+
+Color* GetImageData(Image image)
+{
+    return (Color*)image.data;
+}
+
 DataFile::DataFile()
 {
 	recordCount = 0;
